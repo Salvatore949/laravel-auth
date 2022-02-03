e36c54bcb8efed6189e2287c82c3f524c037804f
Salvatore949 <salvo.mess49@gmail.com>
2022-02-03 12:52:28 +0100

    route created

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'GuestController@home')->name('home');
