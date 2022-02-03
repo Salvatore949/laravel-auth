9a89bad8053b089f3dffb0d653c713da7e229714
Salvatore949 <salvo.mess49@gmail.com>
2022-02-03 13:26:57 +0100

    form created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.main')
 @section('main')
-<h1>body</h1>
+<h1>Log in</h1>
+<form>
+
+    <label for="email"> E-mail</label>
+    <input type="text" name="email"> <br>
+
+    <label for="password"> Password</label>
+    <input type="text" name="password"> <br>
+
+    <input type="submit" value="LOGINS">
+
+</form>
+
 @endsection
